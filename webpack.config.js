361e67dee3074d94a5d9ed969760c100b8f8c313
nerddude <30808467+nerddude24@users.noreply.github.com>
2024-08-02 23:15:30 +0100

    Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+	mode: "development",
+	entry: "./src/index.js",
+	output: {
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "dist"),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.jpg$/i,
+				type: "asset/resource",
+			},
+		],
+	},
+	plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+};
